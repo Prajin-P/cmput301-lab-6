9d3f93734b56b61fb16f5741b26e5107a0d11876
Prajin <prajin@Prajins-MacBook-Pro.local>
2025-10-17 14:10:01 -0600

    Implemented tests and Javadoc

--- ListyCity/app/build.gradle.kts
+++ ListyCity/app/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(files("/Users/prajin/Library/Android/sdk/platforms/android-36/android.jar"))
+
 }
